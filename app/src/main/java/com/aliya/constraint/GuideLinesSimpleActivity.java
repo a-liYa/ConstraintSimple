1501e41c8c9bd8a7ff473a64c40e208af796737b
a_liYa <liyaf@8531.cn>
2017-10-05 18:04:14 +0800

    添加GuideLines使用示例

---
+++ app/src/main/java/com/aliya/constraint/GuideLinesSimpleActivity.java
@@ -0,0 +1,19 @@
+package com.aliya.constraint;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 基线 - Simple
+ *
+ * @author a_liYa
+ * @date 2017/10/5 下午5:37.
+ */
+public class GuideLinesSimpleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide_lines_simple);
+    }
+}
